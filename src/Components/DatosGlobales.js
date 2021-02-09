a68beaab805881ee1e55bf9210a51a66090ec496
javivn99 <javiyosoyyo@gmail.com>
2021-02-09 17:42:53 +0100

    avanzo vacunas

--- src/Components/DatosGlobales.js
+++ src/Components/DatosGlobales.js
@@ -1,11 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import {Modal, ModalBody, ModalFooter, ModalHeader} from 'reactstrap';
 
-const Pricing = () => {
+function Datos_Globales(){
+    const baseUrl = "http://localhost:4004/vacunas/";
+    const [data, setData] = useState([]);
+    const [dosis_entregadas, setDosisEntregadas] = useState(0); //BUSCAR COMO USAR HOOKS CON REACT
+    const [dosis_administradas, setDosisAdministradas] = useState(0);
+    const [personas_pauta_completa, setPersonasPautaCompleta] = useState(0);
+
+    const peticionGet = async () => { //Con esto guardo un array con todos los datos de la bdd.
+        await axios.get(baseUrl)
+            .then(response => {
+                setData(response.data);
+/*
+
+                let d_entregadas = 0;
+                let d_administras = 0;
+                let d_pers_pauta = 0;
+
+                response.data
+                d_entregadas = d_entregadas + vacunas.dosis_entregadas;
+                d_
+                personas_pauta_completa
+*/
+                //console.log(response.data);
+            }).catch(error => {
+                console.log(error);
+            })
+    }//peticionGet
+
+    //Esta linea indica que la primera funcion en ejecutarse sea el get
+    useEffect(() => {
+        peticionGet();
+    }, [])
+
     return (
         <div>
             <h1>Datos globales</h1>
+            <div>
+                <h3>Dosis entregadas en CC.AA</h3>
+                <p></p>
+            </div>
+            <div>
+                <h3>Dosis administradas</h3>
+
+            </div>
+            <div>
+                <h3>Nº Personas con pauta completa</h3>
+
+            </div>
         </div>
     );
 }
 
-export default Pricing;
+export default Datos_Globales;
